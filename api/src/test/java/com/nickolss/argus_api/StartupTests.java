faa66b57fcc01dd4e3419792afdb261962c42804
nickolss <nickolasmaiaraujo@gmail.com>
2025-07-09 17:49:12 -0300

    feat: added project structure

---
+++ api/src/test/java/com/nickolss/argus_api/StartupTests.java
@@ -0,0 +1,13 @@
+package com.nickolss.argus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
